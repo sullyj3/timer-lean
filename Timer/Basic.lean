0774c679eeb0d41628230825599c0e795cfdd849
James Sully <sullyj3@gmail.com>
2024-07-03 12:38:33 +1000

    style tweak

--- Timer/Basic.lean
+++ Timer/Basic.lean
@@ -1,14 +1,14 @@
 open System (FilePath)
 
-def runtimeDir : IO FilePath := do
-  let mRuntimeDir ← IO.getEnv "XDG_RUNTIME_DIR"
-  mRuntimeDir.getDM do
-    IO.eprintln "Error: failed to get XDG_RUNTIME_DIR!"
-    IO.Process.exit 1
+def runtimeDir : IO FilePath := IO.getEnv "XDG_RUNTIME_DIR" >>= 
+  λ | some runtimeDir => return runtimeDir
+    | none => do
+      IO.eprintln "Error: failed to get XDG_RUNTIME_DIR!"
+      IO.Process.exit 1
 
 namespace Timer
 
 def getSockPath : IO FilePath := 
-  return (← runtimeDir).join "timerd.sock"
+  runtimeDir <&> (· / "timerd.sock")
 
 end Timer
