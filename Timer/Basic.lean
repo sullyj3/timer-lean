25ca2462959ad3653cb47c964120315d56dd3611
James Sully <sullyj3@gmail.com>
2024-07-04 20:38:10 +1000

    make some defs private

--- Timer/Basic.lean
+++ Timer/Basic.lean
@@ -1,13 +1,13 @@
 import Socket
 open System (FilePath)
 
-def Option.getOrFail (msg : String) : Option α → IO α
+private def Option.getOrFail (msg : String) : Option α → IO α
 | some x => return x
 | none => do
   IO.eprintln msg
   IO.Process.exit 1
 
-def runtimeDir : IO FilePath := do
+private def runtimeDir : IO FilePath := do
   (← IO.getEnv "XDG_RUNTIME_DIR")
     |>.getOrFail "Error: failed to get XDG_RUNTIME_DIR!"
 
