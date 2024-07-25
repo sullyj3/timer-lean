e14e01f7ae7cb9b322fd374bd4403a70a30b18d7
James Sully <sullyj3@gmail.com>
2024-07-25 15:41:20 +1000

    move lean source files into src

--- lakefile.lean
+++ lakefile.lean
@@ -2,11 +2,14 @@ import Lake
 
 open System Lake DSL
 
-package timer
-
 require socket from git "https://github.com/hargoniX/socket.lean"@"main"
+
+package timer where
+  srcDir := "src"
 
 lean_lib Sand
 
-@[default_target] lean_exe sand where root := `SandClient
-@[default_target] lean_exe sandd where root := `SandDaemon
+@[default_target] lean_exe sand where 
+  root := `SandClient
+@[default_target] lean_exe sandd where 
+  root := `SandDaemon
