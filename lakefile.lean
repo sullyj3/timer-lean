2a51f78f056da07087ea5189d56b0c6736960ad4
James Sully <sullyj3@gmail.com>
2024-07-27 18:08:24 +1000

    fix batteries git URL to unbreak CI

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,7 @@ import Lake
 open System Lake DSL
 
 require socket from git "https://github.com/hargoniX/socket.lean"@"main"
-require batteries from git "git@github.com:leanprover-community/batteries.git"@"v4.9.1"
+require batteries from git "https://github.com/leanprover-community/batteries"@"v4.9.1"
 
 package sand where
   srcDir := "src"
