df692358696f420611a9a7b3bd5ccfc5b78c3ef0
James Sully <sullyj3@gmail.com>
2024-08-05 18:47:44 +1000

    implement version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 && args[1] == "version" {
+        println!("Sand v0.3.0: rewrite it in Rust");
+    } else {
+        println!("Hello, world!");
+    }
 }
