363c0bf9a5b916b6e72a1894e85d78b64c50d1a1
James Sully <sullyj3@gmail.com>
2024-08-05 21:20:58 +1000

    begin daemon main

--- src/client.rs
+++ src/client.rs
@@ -5,23 +5,23 @@ pub fn main(cmd: cli::CliCommand) {
         cli::CliCommand::Start { duration } => {
             let duration_str = duration.join(" ");
             println!("Starting new timer for duration: {}", duration_str);
-            // Implement timer creation functionality
+            unimplemented!();
         }
         cli::CliCommand::Ls => {
             println!("Listing timers...");
-            // Implement list functionality
+            unimplemented!();
         }
         cli::CliCommand::Pause { timer_id } => {
             println!("Pausing timer {}...", timer_id);
-            // Implement pause functionality
+            unimplemented!();
         }
         cli::CliCommand::Resume { timer_id } => {
             println!("Resuming timer {}...", timer_id);
-            // Implement resume functionality
+            unimplemented!();
         }
         cli::CliCommand::Cancel { timer_id } => {
             println!("Cancelling timer {}...", timer_id);
-            // Implement cancel functionality
+            unimplemented!();
         }
         cli::CliCommand::Version => unreachable!("handled in top level main"),
         cli::CliCommand::Daemon(_) => unreachable!("handled in top level main"),
--- src/daemon.rs
+++ src/daemon.rs
@@ -1,6 +1,35 @@
+use std::os::fd::FromRawFd;
+use std::os::fd::RawFd;
+use std::os::unix;
+
 use crate::cli;
 use crate::sand;
 
+const SYSTEMD_SOCKFD: RawFd = 3;
+
+fn env_fd() -> Option<u32> {
+    let str_fd = std::env::var("SAND_SOCKFD").ok()?;
+    let fd = str_fd.parse::<u32>()
+        .expect("Error: Found SAND_SOCKFD but couldn't parse it as a string")
+        .into();
+    Some(fd)
+}
+
 pub fn main(_args: cli::DaemonArgs) {
     println!("Starting sand daemon {}", sand::VERSION);
+
+    let fd: RawFd = match env_fd() {
+        None => {
+            println!("SAND_SOCKFD not found, falling back on default.");
+            SYSTEMD_SOCKFD
+        },
+        Some(fd) => {
+            println!("Found SAND_SOCKFD.");
+            fd.try_into().expect("Error: SAND_SOCKFD is too large to be a file descriptor.")
+        },
+    };
+
+    let _listener = unsafe { unix::net::UnixListener::from_raw_fd(fd) };
+
+    unimplemented!();
 }
