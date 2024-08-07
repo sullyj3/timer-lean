25ce863101540e3dc3329fca7a25cf86c0838092
James Sully <sullyj3@gmail.com>
2024-08-07 23:26:52 +1000

    basic audio working

--- src/daemon.rs
+++ src/daemon.rs
@@ -1,25 +1,26 @@
-mod state;
 mod handle_client;
+mod state;
 
-use std::str;
 use std::io;
 use std::os::fd::FromRawFd;
 use std::os::fd::RawFd;
 use std::os::unix;
 use std::path::Path;
 use std::path::PathBuf;
+use std::str;
 
+use async_scoped;
+use async_scoped::TokioScope;
 use dirs;
 use tokio;
 use tokio::net::UnixListener;
 use tokio::runtime::Runtime;
-use async_scoped;
-use async_scoped::TokioScope;
 
 use crate::cli;
 use crate::sand;
-use state::DaemonCtx;
+use crate::sand::audio::play_notification_sound;
 use handle_client::handle_client;
+use state::DaemonCtx;
 
 const SYSTEMD_SOCKFD: RawFd = 3;
 const SOUND_FILENAME: &str = "timer_sound.opus";
@@ -28,6 +29,7 @@ fn xdg_sand_data_dir() -> Option<PathBuf> {
     Some(dirs::data_dir()?.join("sand"))
 }
 
+// TODO BUG return None if file does not exist
 fn xdg_sound_path() -> Option<PathBuf> {
     Some(xdg_sand_data_dir()?.join(SOUND_FILENAME))
 }
@@ -55,10 +57,10 @@ async fn accept_loop(listener: UnixListener, state: &DaemonCtx) {
         match listener.accept().await {
             Ok((stream, _addr)) => {
                 eprintln!("got client");
-                
+
                 // Todo can we get rid of this clone? maybe if we use scoped threads?
                 let _jh = tokio::spawn(handle_client(stream, state.clone()));
-            },
+            }
             Err(e) => {
                 eprintln!("Error: failed to accept client: {}", e);
                 continue;
@@ -74,7 +76,7 @@ async fn daemon(fd: RawFd, o_sound_path: Option<PathBuf>) -> io::Result<()> {
     std_listener.set_nonblocking(true)?;
     let listener: UnixListener = UnixListener::from_std(std_listener)?;
 
-    TokioScope::scope_and_block(|scope | {
+    TokioScope::scope_and_block(|scope| {
         scope.spawn(accept_loop(listener, &state));
     });
 
@@ -101,6 +103,10 @@ pub fn main(_args: cli::DaemonArgs) -> io::Result<()> {
         eprintln!("Warning: failed to locate notification sound. Audio will not work");
     }
 
+    // todo
+    let sound_path: PathBuf = "/usr/share/sand/timer_sound.flac".into();
+    let sound = sand::audio::Sound::load(sound_path).unwrap();
+    play_notification_sound(&sound).unwrap();
     let rt = Runtime::new()?;
     rt.block_on(daemon(fd, o_sound_path))
 }
--- src/sand.rs
+++ src/sand.rs
@@ -1,8 +1,59 @@
 pub mod cli;
+pub mod duration;
 pub mod message;
 pub mod timer;
 pub mod timers;
-pub mod duration;
 
 pub const VERSION: &str = "Sand v0.3.0: rewrite it in Rust";
 
+pub mod audio {
+    use std::convert::AsRef;
+    use std::fmt::Debug;
+    use std::io::BufReader;
+    use std::io::{self, Read};
+    use std::path::Path;
+    use std::sync::Arc;
+
+    use rodio;
+    use rodio::PlayError;
+
+    // thanks sinesc
+    // https://github.com/RustAudio/rodio/issues/141#issuecomment-383371609
+    pub struct Sound(Arc<Vec<u8>>);
+
+    impl AsRef<[u8]> for Sound {
+        fn as_ref(&self) -> &[u8] {
+            &self.0
+        }
+    }
+
+    impl Sound {
+        pub fn load<P>(path: P) -> io::Result<Sound>
+        where
+            P: AsRef<Path> + Debug,
+        {
+            use std::fs::File;
+            let mut buf = Vec::new();
+            let mut file = File::open(path)?;
+            file.read_to_end(&mut buf)?;
+            Ok(Sound(Arc::new(buf)))
+        }
+
+        pub fn cursor(self: &Self) -> io::Cursor<Sound> {
+            io::Cursor::new(Sound(self.0.clone()))
+        }
+
+        pub fn decoder(self: &Self) -> rodio::Decoder<io::Cursor<Sound>> {
+            rodio::Decoder::new(self.cursor()).unwrap()
+        }
+    }
+
+    pub fn play_notification_sound(sound: &Sound) -> Result<(), PlayError> {
+        let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+        let sink = rodio::Sink::try_new(&handle)?;
+        sink.append(sound.decoder());
+        sink.sleep_until_end();
+        eprintln!("notification sound");
+        Ok(())
+    }
+}
